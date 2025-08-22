e01714f601626b5b1318d86e100634b5a8d0d33e
Your Name <you@example.com>
2025-08-22 16:29:14 +0400

    ?2

--- rtl/top_gowin.v
+++ rtl/top_gowin.v
@@ -19,7 +19,7 @@ module top_gowin (
   );
 
   wire serial_out;
-    assign serial_out = 1;
+  assign serial_out = 1;
   TLVDS_OBUF tmds_bufds (
       .I (serial_out),
       .O (serial_out_diff_p),
@@ -28,27 +28,90 @@ module top_gowin (
 
 
 
-  reg [5:0] cnt;
-  reg [55:0] data;
-  reg data_bit;
-  ODDR oddr1 (
-      .CLK(clk108),
-      .D0 (~data_bit),
-      .D1 (data_bit),
-      .Q0 (test_clk),
-      .TX (1'b1)
+  // reg [5:0] cnt;
+  // reg [55:0] data;
+  // reg data_bit;
+  // ODDR oddr1 (
+  //     .CLK(clk108),
+  //     .D0 (~data_bit),
+  //     .D1 (data_bit),
+  //     .Q0 (test_clk),
+  //     .TX (1'b1)
+  // );
+  // always @(posedge clk108) begin
+  //   if (!aresetn) begin
+  //     cnt <= 0;
+  //     data <= 56'hAAAAD5AABBCCEE;
+  //     data_bit <= 0;
+  //   end else begin
+  //     data_bit <= data[55-cnt];
+  //     cnt <= (cnt == 55) ? 0 : cnt + 1'b1;
+
+  //   end
+  // end
+  reg [1:0] ce_div;
+  always @(posedge clk108 or negedge aresetn) begin
+    if (!aresetn) ce_div <= 2'b00;
+    else ce_div <= ce_div + 2'd1;
+  end
+
+  wire pclk_ce_clk = (ce_div == 2'b00);
+  wire pclk_ce;
+   BUFG bufg1 (.I(pclk_ce_clk), .O(pclk_ce));
+
+  reg [7:0] data[0:6];
+  reg [2:0] cnt;
+  reg [7:0] data_8;
+  wire [15:0] man_out;
+  wire [7:0] data_out = (second_word) ? man_out[15:8] : man_out[7:0];
+  reg second_word;
+  manchester_encoder me_1 (
+      .data_in(data_8),
+      .manchester_out(man_out)
   );
-  always @(posedge clk108) begin
+  initial begin
+    data[0] = 8'hAA;
+    data[1] = 8'hAA;
+    data[2] = 8'hD5;
+    data[3] = 8'hAA;
+    data[4] = 8'hBB;
+    data[5] = 8'hCC;
+    data[6] = 8'hFF;
+  end
+
+
+  always @(posedge pclk_ce) begin
     if (!aresetn) begin
       cnt <= 0;
-      data <= 56'hAAAAD5AABBCCDD;
-      data_bit <= 0;
+      second_word <= 0;
     end else begin
-      data_bit <= data[55-cnt];
-      cnt <= (cnt == 55) ? 0 : cnt + 1'b1;
-
+      cnt <= (cnt == 6) ? 0 : cnt + 1'b1;
+      data_8 <= data[cnt];
+      if (cnt == 6) begin
+        second_word <= ~second_word;
+      end
     end
   end
+
+  OSER8 ser8_1 (
+      .Q0(test_clk),
+      .Q1(),
+      .D0(data_out[0]),
+      .D1(data_out[1]),
+      .D2(data_out[2]),
+      .D3(data_out[3]),
+      .D4(data_out[4]),
+      .D5(data_out[5]),
+      .D6(data_out[6]),
+      .D7(data_out[7]),
+      .TX0(1'b1),
+      .TX1(1'b1),
+      .TX2(1'b1),
+      .TX3(1'b1),
+      .PCLK(pclk_ce),
+      .FCLK(clk108),
+      .RESET(~aresetn)
+  );
 
 
 
